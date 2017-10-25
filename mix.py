01213a3eb3a0a4615799e8b2c7d9e993c642dc6b
sxfan <sxfan@e33d6d0a-b5ac-4242-812c-e48df55b6677>
2017-10-25 23:05:24 +0000

    add mix.py
    git-svn-id: svn://192.168.1.3/avsync@3 e33d6d0a-b5ac-4242-812c-e48df55b6677

---
+++ mix.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import scipy.io.wavfile
+import numpy as np
+from subprocess import call
+import math
+import os
+import argparse
+
+cmd_line = ["ffmpeg"]
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('audio', help='audio file ./path/audio_name.ext')
+	parser.add_argument('video', help='video file ./path/video_name.ext')
+	parser.add_argument('-a', help='pop sound start second in audio file ')
+	parser.add_argument('-v', help='pop sound start second in video file ')
+	parser.add_argument('-t', help='final video duration in second')
+	parser.add_argument('-m', help='iOS compatible with iPhone4/iPad/Apple TV 2', action="store_true")
+	
+	args = parser.parse_args()
+	if args.v :
+		v_start = args.v
+	if args.a :
+		a_start = args.a
+	if args.t :
+		len = args.t
+		cmd_line += ["-t", len]
+
+	av_audio = args.audio
+	av_video = args.video
+
+	if not args.a and not arg.v:
+		a_start, v_start = align(av_audio, av_video)
+
+	filename, file_ext = os.path.splitext(av_audio)
+	output = filename + ".mp4"
+	
+	cmd_line += ["-ss", a_start, "-i", av_audio, "-ss", v_start, "-i", av_video]
+	cmd_line +=	["-c:v", "libx264", "-crf", "18"]
+	if args.m :
+		cmd_line += ["-profile:v", "main", "-level", "3.1", "-vf", "scale=iw/2:-1"]
+	cmd_line += ["-c:a", "aac", "-b:a", "256k", "-strict", "-2"]
+	cmd_line += ["-map", "0:a", "-map", "1:v", "-y", output]
+
+	print ' '.join(str(e) for e in cmd_line)
+#	call(cmd_line)
+# ./mix.py "./media/DSC_3554_Ryan_1.wav" "./media/DSC_3554.MOV" -a 9.535 -v 4.634 -t 57 -m
+# ffmpeg -y -t 57 -ss 4.634 -i DSC_3554.MOV -ss 9.535 -i DSC_3554_Ryan_1.wav -c:v libx264 -crf 18 -c:a aac -b:a 256k -map 0:v -map 1:a -shortest -strict -2 DSC_3554_Av.MOV
