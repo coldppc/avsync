ffbf86d0f605c75339967fb9a56a4ff026547b97
sxfan <sxfan@e33d6d0a-b5ac-4242-812c-e48df55b6677>
2017-11-16 00:45:50 +0000

    add mice.py, recording script
    git-svn-id: svn://192.168.1.3/avsync@12 e33d6d0a-b5ac-4242-812c-e48df55b6677

---
+++ mice.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import sys
+import struct
+from subprocess import call
+import threading
+import time
+import Queue
+import select
+import signal, os
+from datetime import datetime
+
+mice_q = Queue.Queue()
+
+alsa_pid = '/tmp/alsa.pid '
+global gExit
+gExit = False
+
+global gState
+gState = 'IDLE' # idle,  play, record
+
+global last_wav
+last_wav = ''
+
+def handler(signum, frame):
+	global gExit
+	print 'Signal handler called with signal', signum
+	gExit = True
+
+def get_mice_event(f_mice):
+	buf = f_mice.read(3)
+	button = ord( buf[0] )
+	bLeft = (button & 0x1) > 0
+	bMiddle = ( button & 0x4 ) > 0
+	bRight = ( button & 0x2 ) > 0
+	x,y = struct.unpack( "bb", buf[1:] )
+	#print ("L:%d, M: %d, R: %d, x: %d, y: %d\n" % (bLeft, bMiddle, bRight, x, y) )
+	return bLeft, bRight
+
+def aplay_cmd():
+	cmd = 'sudo aplay -v -D hw:CARD=USB,DEV=0 -i --process-id-file ' + alsa_pid
+	cmd += last_wav
+	return cmd
+	
+def arecord_cmd() :
+	global last_wav
+	dt = datetime.now()
+	cmd = 'sudo arecord -v -f S32_LE -c 2 -r 48000 -D hw:CARD=USB,DEV=0 --process-id-file ' + alsa_pid
+	last_wav = dt.strftime("%Y%m%d_%H%M%S.wav")
+	cmd += last_wav
+	return cmd
+
+def kill_cmd():
+	cmd = 'sudo kill -9 `cat ' + alsa_pid + '`'
+	return cmd
+
+def worker_cmd(cmd):
+	worker = sys._getframe().f_code.co_name
+	print worker + " starting..."
+	print cmd
+	call (cmd, shell=True)
+	print worker + " done!"
+	
+def exec_cmd(cmd):
+	t_cmd = threading.Thread( target = worker_cmd, args = (cmd,))
+	t_cmd.start()
+	
+def state_machine(left, right) :
+	global gState
+	state_old = gState
+	if gState == 'IDLE' :
+		if left and not right :
+			gState = 'RECORD'
+			cmd_line = arecord_cmd()
+			exec_cmd(cmd_line)
+		elif left and right :
+			if last_wav == '' : # nothing to play
+				return
+			gState = 'PLAY'
+			cmd_line = aplay_cmd()
+			exec_cmd(cmd_line)
+	elif gState == 'RECORD' :
+		if right and not left :
+			gState = 'IDLE'
+			cmd_line = kill_cmd()
+			exec_cmd(cmd_line)
+	elif gState == 'PLAY' :
+		if left and right :
+			gState = 'IDLE'
+			cmd_line = kill_cmd()
+			exec_cmd(cmd_line)
+	else :
+		return
+	if gState != state_old :
+		print gState
+
+def worker_ctrl():
+	worker = sys._getframe().f_code.co_name
+	print worker + " starting..."
+	while not gExit:
+		while not mice_q.empty() :
+			left, right = mice_q.get()
+			print "LEFT/RIGHT: ", left, right
+			state_machine(left, right)
+			mice_q.task_done
+	print worker + " done!"
+
+def worker_mice():
+	worker = sys._getframe().f_code.co_name
+	print worker + " starting..."
+	f_mice = open( "/dev/input/mice", "rb" )
+	left = left_old = False
+	right = right_old = False
+	timeout = 0.2
+	while not gExit:
+		rlist, wlist, xlist = select.select([f_mice.fileno()], [], [], timeout)
+		if len(rlist) == 0 :
+			# timeout, key is stable
+			if (left_old != left) or (right_old != right) :
+				# generate event only when something changed
+				mice_q.put( (left,right) )
+				left_old = left
+				right_old = right
+		else:
+			# readable
+			left, right = get_mice_event(f_mice)
+			
+	f_mice.close()
+	print worker + " done!"
+
+
+if __name__ == '__main__':
+
+	signal.signal(signal.SIGTERM, handler)
+	signal.signal(signal.SIGINT, handler)
+	
+	t_ctrl = threading.Thread( target = worker_ctrl )
+	t_mice = threading.Thread( target = worker_mice )
+	t_mice.start()
+	t_ctrl.start()
+	
+	while not gExit :
+		time.sleep(1)
