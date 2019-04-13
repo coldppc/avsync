d00199360e38da13a8151a0bf32b273e9ef1c969
sxfan <sxfan@e33d6d0a-b5ac-4242-812c-e48df55b6677>
2019-04-13 05:49:09 +0000

    batch process clips, x264 + pcm --> .mov
    git-svn-id: svn://192.168.1.3/avsync@19 e33d6d0a-b5ac-4242-812c-e48df55b6677

--- align.py
+++ align.py
@@ -3,7 +3,8 @@ import scipy.io.wavfile
 import numpy as np
 from subprocess import call
 import math
-import os
+import re
+import os, glob
 import argparse
 
 # Extract audio from video file, convert to fixed sample rate mono
@@ -138,65 +139,160 @@ def align(av_base, av_file, fft_bin_size=1024, overlap=0, box_height=512, box_wi
 	#print ft_dict2
 
 	# Determie time delay
-	print "======================= Determie time delay ============================="
+	print "================================ Determie time offset ================================="
+
 	pairs = find_freq_pairs(ft_dict1, ft_dict2)
 	#print pairs
 	delay = find_delay(pairs)
 	samples_per_sec = float(rate) / float(fft_bin_size)
 	seconds= round(float(delay) / float(samples_per_sec), 4)
 
-	if seconds > 0:
-		return (0, seconds)
+	print "====================== Audio is ahead of video %8.4f seconds =======================" % -(seconds)
+	# remove temp files
+	tmp_files = glob.glob("*_MONO.wav")
+	for tmp_file in tmp_files:
+		print "removing", tmp_file, "..."
+		os.remove(tmp_file)
+
+	return -(seconds)
+
+def key_frame_time(v_file):
+	key_list = []
+	key_file = v_file + ".key"
+	if not os.path.isfile(key_file):
+		print "Analyzing %s for key frames ..." % v_file
+		cmd_line = ["ffprobe", "-v", "error", "-skip_frame", "nokey", "-select_streams", "v:0", 
+		"-show_entries", "frame=pkt_pts_time", "-of", "csv=print_section=0", v_file]
+		print ' '.join(str(e) for e in cmd_line)
+		with open(key_file, 'w') as f:
+			call(cmd_line, stdout=f)
+	
+	if os.path.isfile(key_file):
+		print "Load key frame positions from file %s" % key_file
+		key_list = np.loadtxt(key_file)
+
+	print "Found %d key frames in video %s" % (len(key_list), v_file)
+	return key_list
+
+# convert to seconds, "00:22.6"->22.6, "00:01:22.6"->82.6, "01:01:22.6"->3682.6, "22.6"->22.6
+def time_to_second(time_str):
+	times = map(float, time_str.split(":"))
+	if len(times) == 3:
+		t = times[0] * 3600 + times[1] * 60 + times[2]
+	elif len(times) == 2:
+		t = times[0] * 60 + times[1]
+	elif len(times) == 1:
+		t = times[0]
 	else:
-		return (abs(seconds), 0 )
+		t = 0.0
+	return t
 
+def nearest_key_frame(key_list, t):
+	p = 0
+	while p+1 < len(key_list) and t > key_list[p+1] :
+		p += 1
+	return key_list[p]
+'''
+def nearest_key_frame(key_list, t):
+	p = 0
+	while p < len(key_list) and key_list[p] < t :
+		p += 1
+	return key_list[p]
+'''	
+def clip_encode(a_start, v_start, v_end, audio, video, output, re_encode=False, mobile_version=False, extra=""):
+	# extract video
+	# ffmpeg -ss 663.245417 -i DSC_5661.MOV -t 229.254583 -c:v copy -an -avoid_negative_ts make_zero -y temp_video.mov
+	cmd_line = ["ffmpeg"]
+	cmd_line += ["-ss", str(v_start), "-i", video, "-t", str(v_end - v_start)]
 
-cmd_line = ["ffmpeg"]
+	if re_encode:
+		cmd_line +=	["-c:v", "libx264", "-crf", "18"]
+		# GOP default 250 -> 120 (multiple of 24, 30, 60)
+		cmd_line += ["-tune", "film", "-g", "120"]
+		if mobile_version :
+			cmd_line += ["-profile:v", "main", "-level", "3.1", "-vf", "scale=iw/2:-2"]
+	else:
+		cmd_line +=	["-c:v", "copy"]
+
+	cmd_line += ['-an', '-avoid_negative_ts', 'make_zero']
+	cmd_line += ["-y", "temp_video.mov"]
+
+	print ' '.join(str(e) for e in cmd_line)
+	call(cmd_line)
+
+	# extract audio
+	# ffmpeg -ss 666.344517 -i 190407_DSC_5661V.wav -t 229.254583 -c: copy -y temp_audio.wav
+	cmd_line = ["ffmpeg"]
+	cmd_line += ["-ss", str(a_start), "-i", audio, "-t", str(v_end - v_start)]
+
+	#cmd_line += ["-c:a", "aac", "-b:a", "256k", "-strict", "-2"]
+	cmd_line += ["-c:a", "copy"]
+	cmd_line += ["-y", "temp_audio.wav"]
+
+	print ' '.join(str(e) for e in cmd_line)
+	call(cmd_line)
+
+	# mux audio and video
+	# ffmpeg -i audio_only.wav -i video_only.mov -c:a copy -c:v copy -map 1:v -map 0:a -y -shortest audio_video.mov
+	cmd_line = ["ffmpeg"]
+	cmd_line += ["-i", "temp_audio.wav", "-i", "temp_video.mov", "-c", "copy", "-map", "1:v", "-map", "0:a", "-shortest"]
+	
+	if extra != "":
+		extra_opts = extra.split()
+		cmd_line += extra_opts
+
+	cmd_line += ["-y", output]
+
+	print ' '.join(str(e) for e in cmd_line)
+	call(cmd_line)
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('audio', help='audio file ./path/audio_name.ext')
 	parser.add_argument('video', help='video file ./path/video_name.ext')
 	parser.add_argument('-a_delay', help='adjust audio output delay. (default=0.0417 24fps)', dest='a_delay', default=0.0417)
-	parser.add_argument('-s', help='offset of video start in second')
-	parser.add_argument('-t', help='final video duration in second')
+	parser.add_argument('-r', help='re-encode video using libx264')
+	parser.add_argument('-c', '--clip', action='append', help='video clips in format: 00:00.000-59:59.999')
 	parser.add_argument('-m', help='iOS compatible with iPhone4/iPad/Apple TV 2', action="store_true")
+	parser.add_argument('-e', help='extra options', default="")
 
 	args = parser.parse_args()
 
-	offset = "0.0"
-	if args.s :
-		offset = args.s
+	re_encode = False
 
 	av_audio = args.audio
 	av_video = args.video
 
-	a_start, v_start = align(av_audio, av_video)
-	a_start += float(offset);
-	v_start += float(offset);
+	key_list = key_frame_time(av_video)
 
-	if args.a_delay :
-		a_start -= float(args.a_delay)
+	a_delay = align(av_audio, av_video)
+	a_delay -= 0.5005 # FIXME !!!
 
-	filename, file_ext = os.path.splitext(av_audio)
-	if args.m :
-		filename += ".m"
-	output = filename + ".mp4"
+	if a_delay < 0 :
+		print "============================= Audio should cover video ! =============================="
+		quit()
+	if args.m or args.r:
+		print "============================= Video will be re-encoded ! =============================="
+		re_encode = True
 
-	cmd_line += ["-ss", str(a_start), "-i", av_audio, "-ss", str(v_start)]
+	a_name, a_ext = os.path.splitext(av_audio)
+	v_name, v_ext = os.path.splitext(av_video)
+	for clip in args.clip:
+		clip_start, clip_end = clip.split('-')
+		t_clip_start = time_to_second(clip_start)
+		v_start = nearest_key_frame(key_list, t_clip_start)
+		t_clip_end = time_to_second(clip_end)
+		#print "---------------------> t_clip_start = %f, v_start = %f, t_clip_end= %f" % (t_clip_start, v_start, t_clip_end)
+		output = a_name + "_" + v_name + "_%06.1f-%06.1f" % (v_start, t_clip_end)
+		if args.m :
+			output += ".m"
 
-	cmd_line +=	["-i", av_video, "-c:v", "libx264", "-crf", "18"]
-	if args.m :
-		cmd_line += ["-profile:v", "main", "-level", "3.1", "-vf", "scale=iw/2:-1"]
-	cmd_line += ["-c:a", "aac", "-b:a", "256k", "-strict", "-2"]
+		output += ".mov"
+		
+		clip_encode(a_delay+v_start, v_start, t_clip_end, av_audio, av_video, output, re_encode, True if args.m else False, args.e)
 
-	if args.t :
-		out_len = args.t
-		cmd_line += ["-t", out_len]
-
-	cmd_line += ["-map", "0:a", "-map", "1:v", "-y", output]
-
-	print ' '.join(str(e) for e in cmd_line)
-	call(cmd_line)
-
-	# ./align.py "./media/DSC_3554_Ryan_1.wav" "./media/DSC_3554.MOV"
-	# ./align.py ./media/DSC_3555_Ryan_2.wav ./media/DSC_3555.MOV
+	# remove temp files
+	tmp_files = glob.glob("temp*.*")
+	for tmp_file in tmp_files:
+		print "removing", tmp_file, "..."
+		os.remove(tmp_file)
